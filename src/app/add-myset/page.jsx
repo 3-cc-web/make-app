001068467051848e7536f6cb38d7b11116896ca0
sprout-hatanaka <sprout.hatanaka@gmail.com>
2026-03-12 21:04:06 +0900

    商品情報を取得

--- src/app/add-myset/page.jsx
+++ src/app/add-myset/page.jsx
@@ -11,6 +11,7 @@ export default function AddMyset() {
   const [name, setName] = useState("");
   const [categories, setCategories] = useState(null);
   const [category, setCategory] = useState(null);
+  const [product, setProduct] = useState([]);
   const [categoryItems, setCategoryItems] = useState([]);
   const [item, setItem] = useState([]);
   const [myItems, setMyItems] = useState([]);
@@ -18,22 +19,62 @@ export default function AddMyset() {
   // モーダル
   const [isOpen, setIsOpen] = useState(false);
 
+    const fetchProduct = async () => {
+      const {data, error} = await supabase
+        .from('product')
+        .select('*')
 
-  // カテゴリ一覧を取得
-  const fetchCategories = async () => {
-    const {data} = await supabase
-      .from('category')
-      .select(`
-        id,
-        name
-      `)
-      .order('id', {ascending: true})
-    setCategories(data || [])
-  }
+      console.log("data:", data)
+      console.log("error:", error)
+
+      setProduct(data || [])
+    }
+
 
   useEffect(() => {
+    // カテゴリ一覧を取得
+    const fetchCategories = async () => {
+      const {data} = await supabase
+        .from('category')
+        .select(`
+          id,
+          name
+        `)
+        .order('id', {ascending: true})
+      setCategories(data || [])
+    }
     fetchCategories()
+
+    // 商品一覧を取得
+    // const fetchProduct = async () => {
+    //   const {data} = await supabase
+    //     .from('product')
+    //     .select(`
+    //       *
+    //     `)
+    //     .order('id', {ascending: true})
+    //   setProduct(data || [])
+    //   console.log(data)
+    // }
+    // fetchProduct()
   }, [])
+
+  useEffect(() => {
+    fetchProduct()
+  }, [])
+
+// console.log(product);
+
+// // ストレージの画像URLを取得する関数
+// export const getImageUrl = (imgPath) => {
+//   if(!imgPath) return null;
+
+//   const { data } = supabase.storage
+//     .from('clothes_image')
+//     .getPublicUrl(imgPath)
+
+//   return data.publicUrl
+// }
 
   // // そのカテゴリの商品を取得
   // const fetchCategoryItems = async () => {
@@ -89,6 +130,7 @@ export default function AddMyset() {
           {
             categories ? (
               <select className="border" onChange={(e) => setCategory(e.target.value)}>
+                <option value="0">All</option>
                 {
                   categories.map((c) => (
                     <option key={c.id} value={c.id}>
@@ -104,10 +146,27 @@ export default function AddMyset() {
 
 
       <p>選択されてるカテゴリ:{category}</p>
-      {/* {categoryItems.map((ci) => (
-        <radio key={ci.id} value={ci.id} onChange={() => setItem(ci.id)} name="categoryItems">{ci.name}</radio>
-      ))} */}
-      
+
+          {
+            product.length > 0 ? (
+              product.map((p) => (
+                <label key={p.id}>
+                  <input
+                    type="radio"
+                    value={p.id}
+                    name="products"
+                    onChange={() => setItem(p.id)}
+                  />
+                  {p.name}
+                  {/* <Image src={getImageUrl(cl.img_path)} alt='アイテム画像' width={100} height={100}
+                    loading="lazy"
+                    sizes="100px" /> */}
+              </label>
+              ))  
+            ) : (
+              <p>商品情報を取得中…</p>
+            )
+          }   
 
     </>
   )
